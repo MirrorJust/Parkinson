e6843b9a737a039f1547a0d956f6b31fe7eeaf52
MirrorJust <mirrornear@gmail.com>
2020-10-03 02:10:03 +0300

    project with django first

--- userCabinet/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsercabinetConfig(AppConfig):
-    name = 'userCabinet'
--- userCabinet/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('/page/profile/user-main-profile-page.html', views.index, name='userProfile')
-]
